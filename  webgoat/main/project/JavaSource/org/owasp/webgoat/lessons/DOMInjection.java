ba38b57a44cd891f815e7443045870326aa5955f
sherif.fathy <sherif.fathy@4033779f-a91e-0410-96ef-6bf7bf53c507>
2006-12-29 16:43:59 +0000

    Modified DOMInjection per Bruce's comments
    git-svn-id: http://webgoat.googlecode.com/svn/trunk@54 4033779f-a91e-0410-96ef-6bf7bf53c507

---  webgoat/main/project/JavaSource/org/owasp/webgoat/lessons/DOMInjection.java
+++  webgoat/main/project/JavaSource/org/owasp/webgoat/lessons/DOMInjection.java
@@ -130,7 +130,7 @@ public class DOMInjection extends LessonAdapter {
 		List<String> hints = new ArrayList<String>();
 		hints.add( "This page is using XMLHTTP to comunicate with the server." );
 		hints.add( "Try to find a way to inject the DOM to enable the Activate button." );
-		hints.add( "Intercept the reply and add document.forms[0].SUBMIT.disabled = false;" );
+		hints.add( "Intercept the reply and replace the body withx document.forms[0].SUBMIT.disabled = false;" );
 		return hints;
 	}
 
