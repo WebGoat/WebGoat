404e3f8b197bcd389fc2033ff49dd3d52eebe674
esheri3 <esheri3@4033779f-a91e-0410-96ef-6bf7bf53c507>
2007-01-26 19:47:54 +0000

    Changed title to "How to Perform Cross Site Tracing (XST) Attacks"
    git-svn-id: http://webgoat.googlecode.com/svn/trunk@103 4033779f-a91e-0410-96ef-6bf7bf53c507

---  webgoat/main/project/JavaSource/org/owasp/webgoat/lessons/TraceXSS.java
+++  webgoat/main/project/JavaSource/org/owasp/webgoat/lessons/TraceXSS.java
@@ -281,6 +281,6 @@ public class TraceXSS extends LessonAdapter
      */
     public String getTitle()
     {
-	return ("How to Perform Cross Site Trace (XST) Attacks");
+	return ("How to Perform Cross Site Tracing (XST) Attacks");
     }
 }
