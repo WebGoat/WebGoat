45a3477809e6ba2d6654fabb5a73db18eb6b3bd9
wirth.marcel <wirth.marcel@4033779f-a91e-0410-96ef-6bf7bf53c507>
2008-04-09 11:49:56 +0000

    MultiLevel Login1 fix
    git-svn-id: http://webgoat.googlecode.com/svn/trunk/webgoat@305 4033779f-a91e-0410-96ef-6bf7bf53c507

--- main/project/JavaSource/org/owasp/webgoat/lessons/MultiLevelLogin1.java
+++ main/project/JavaSource/org/owasp/webgoat/lessons/MultiLevelLogin1.java
@@ -97,7 +97,7 @@ public class MultiLevelLogin1 extends SequentialLessonAdapter
 	 * See if the user had used a valid tan
 	 * 
 	 * @param s
-	 * @return treu if correctTan
+	 * @return true if correctTan
 	 */
 	private boolean correctTan(WebSession s)
 	{
@@ -143,7 +143,26 @@ public class MultiLevelLogin1 extends SequentialLessonAdapter
 			style sty = new style();
 
 			sty
-					.addElement("#lesson_wrapper {height: 435px;width: 500px;}#lesson_header {background-image: url(lessons/DBSQLInjection/images/lesson1_header.jpg);width: 490px;padding-right: 10px;padding-top: 60px;background-repeat: no-repeat;}.lesson_workspace {background-image: url(lessons/DBSQLInjection/images/lesson1_workspace.jpg);width: 489px;height: 325px;padding-left: 10px;padding-top: 10px;background-repeat: no-repeat;}		.lesson_text {height: 240px;width: 460px;padding-top: 5px;}			#lesson_buttons_bottom {height: 20px;width: 460px;}			#lesson_b_b_left {width: 300px;float: left;}			#lesson_b_b_right input {width: 100px;float: right;}			.lesson_title_box {height: 20px;width: 420px;padding-left: 30px;}			.lesson_workspace { }			.lesson_txt_10 {font-family: Arial, Helvetica, sans-serif;font-size: 10px;}			.lesson_text_db {color: #0066FF}			#lesson_login {background-image: url(lessons/DBSQLInjection/images/lesson1_loginWindow.jpg);height: 124px;width: 311px;background-repeat: no-repeat;padding-top: 30px;margin-left: 80px;margin-top: 50px;text-align: center;}			#lesson_login_txt {font-family: Arial, Helvetica, sans-serif;font-size: 12px;text-align: center;}			#lesson_search {background-image: url(lessons/DBSQLInjection/images/lesson1_SearchWindow.jpg);height: 124px;width: 311px;background-repeat: no-repeat;padding-top: 30px;margin-left: 80px;margin-top: 50px;text-align: center;}");
+					.addElement("#lesson_wrapper {height: 435px;width: " +
+							"500px;}#lesson_header {background-image: " +
+							"url(lessons/DBSQLInjection/images/lesson1_header.jpg);width:" +
+							" 490px;padding-right: 10px;padding-top: 60px;background-repeat: no-repeat;}.lesson_workspace " +
+							"{background-image: url(lessons/DBSQLInjection/images/lesson1_workspace.jpg);width: 489px;height: " +
+							"325px;padding-left: 10px;padding-top: 10px;background-repeat: no-repeat;}		" +
+							".lesson_text {height: 240px;width: 460px;padding-top: 5px;}			" +
+							"#lesson_buttons_bottom {height: 20px;width: 460px;}			" +
+							"#lesson_b_b_left {width: 300px;float: left;}			" +
+							"#lesson_b_b_right input {width: 100px;float: right;}			" +
+							".lesson_title_box {height: 20px;width: 420px;padding-left: 30px;}			" +
+							".lesson_workspace { }			" +
+							".lesson_txt_10 {font-family: Arial, Helvetica, sans-serif;font-size: 10px;}			" +
+							".lesson_text_db {color: #0066FF}			" +
+							"#lesson_login {background-image: url(lessons/DBSQLInjection/images/lesson1_loginWindow.jpg);height: " +
+							"124px;width: 311px;background-repeat: no-repeat;padding-top: 30px;margin-left: 80px;margin-top:" +
+							" 50px;text-align: center;}			#lesson_login_txt {font-family: Arial, Helvetica, sans-serif;font-size: " +
+							"12px;text-align: center;}			#lesson_search {background-image: " +
+							"url(lessons/DBSQLInjection/images/lesson1_SearchWindow.jpg);height: 124px;width: 311px;background-repeat: " +
+							"no-repeat;padding-top: 30px;margin-left: 80px;margin-top: 50px;text-align: center;}");
 			ec.addElement(sty);
 
 			Div wrapperDiv = new Div();
