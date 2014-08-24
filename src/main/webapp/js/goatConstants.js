3aa6cb044b2b9eaa5904b097eb0170eb8111b8ee
Jason White <jsnsotheracct@gmail.com>
2014-08-24 09:04:07 -0400

    removing Home/about/contact from left menu rendering/constants file

--- src/main/webapp/js/goatConstants.js
+++ src/main/webapp/js/goatConstants.js
@@ -4,37 +4,12 @@ var goatConstants = {};
 
 goatConstants.CATEGORYCLASS = 'fa-angle-right pull-right';
 goatConstants.menuPrefix = [
-	{	
-		name:'Home',
-		type:'STATIC',
-		complete:false,
-		link:'home.jsp',
-		children:null,
-		class:'fa-home'
-	},
-	{
-		name:'About',
-		 type:'STATIC',
-		 complete:false,
-		 link:'about.jsp',
-		 children:null,
-		 class:'fa-users'
-	},
-	{
-		name:'Contact',
-		type:'STATIC',
-		complete:false,
-		link:'contact.jsp',
-		children:null,
-		class:'fa-envelope-o'
-	},
 		{
 		name:'LESSONS',
 		type:'STATIC',
 		complete:false,
 		link:'',
 		children:null,
-		class:'fa-bars'
+		class:'fa-bars static'
 	}
-
 ];	
