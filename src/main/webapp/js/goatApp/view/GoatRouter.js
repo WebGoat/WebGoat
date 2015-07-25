482267129ce0b47312aa0526005182b1a1db25ef
Nanne Baars <nbaars@xebia.com>
2015-07-25 17:43:54 +0200

    Fixing Windows issue with goatRouter --> GoatRouter

--- src/main/webapp/js/goatApp/view/GoatRouter.js
+++ src/main/webapp/js/goatApp/view/GoatRouter.js
@@ -1,50 +1,50 @@
 define(['jquery',
-	'underscore',
-	'backbone',
-	'goatApp/controller/LessonController',
-	'goatApp/controller/MenuController',
-	'goatApp/view/LessonContentView',
-	'goatApp/view/MenuView',
-	'goatApp/view/TitleView'
-	], function ($,_,Backbone,LessonController,MenuController,LessonContentView,MenuView,TitleView) {
+    'underscore',
+    'backbone',
+    'goatApp/controller/LessonController',
+    'goatApp/controller/MenuController',
+    'goatApp/view/LessonContentView',
+    'goatApp/view/MenuView',
+    'goatApp/view/TitleView'
+], function ($,_,Backbone,LessonController,MenuController,LessonContentView,MenuView,TitleView) {
 
-		var lessonView = new LessonContentView();
-		var menuView = new MenuView(); 
-		var titleView = new TitleView();
+    var lessonView = new LessonContentView();
+    var menuView = new MenuView();
+    var titleView = new TitleView();
 
-		var GoatAppRouter = Backbone.Router.extend({
-			routes: {
-				//#....
-				'welcome':'welcomeRoute',
-				'attack/:scr/:menu':'attackRoute' //	
-		 	},
-			lessonController: new LessonController({
-				lessonView:lessonView
-			}),
-			menuController: new MenuController({
-				menuView:menuView,
-				titleView:titleView
-			}),
+    var GoatAppRouter = Backbone.Router.extend({
+        routes: {
+            //#....
+            'welcome':'welcomeRoute',
+            'attack/:scr/:menu':'attackRoute' //
+        },
+        lessonController: new LessonController({
+            lessonView:lessonView
+        }),
+        menuController: new MenuController({
+            menuView:menuView,
+            titleView:titleView
+        }),
 
-			init:function() {
-				goatRouter =  new GoatAppRouter();
-				this.lessonController.start();
-				this.menuController.initMenu();
+        init:function() {
+            goatRouter =  new GoatAppRouter();
+            this.lessonController.start();
+            this.menuController.initMenu();
 
-				goatRouter.on('route:attackRoute', function(scr,menu) {
-					console.log('attack route');
-					this.lessonController.loadLesson(scr,menu);
-					this.menuController.updateMenu(scr,menu);
-					//update menu
-				});
-				goatRouter.on('route:welcomeRoute', function() {
-					alert('welcome route');
-				});
-				
-				Backbone.history.start();
-			}
-		});
+            goatRouter.on('route:attackRoute', function(scr,menu) {
+                console.log('attack route');
+                this.lessonController.loadLesson(scr,menu);
+                this.menuController.updateMenu(scr,menu);
+                //update menu
+            });
+            goatRouter.on('route:welcomeRoute', function() {
+                alert('welcome route');
+            });
 
-		return GoatAppRouter;
+            Backbone.history.start();
+        }
+    });
+
+    return GoatAppRouter;
 
 });
