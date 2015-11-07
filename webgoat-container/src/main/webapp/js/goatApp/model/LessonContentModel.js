ea1d852cda49e692ac58054ab292edfb384468a1
Ilguiz Latypov <ilatypov@yahoo.ca>
2015-11-07 03:59:12 -0500

    Convert the message number parameter into the MVC route part.  Correct the result of the restart lesson button.

--- webgoat-container/src/main/webapp/js/goatApp/model/LessonContentModel.js
+++ webgoat-container/src/main/webapp/js/goatApp/model/LessonContentModel.js
@@ -1,46 +1,54 @@
 define(['jquery',
-	'underscore',
-	'backbone',
-	'goatApp/model/HTMLContentModel'],
-	 function($,
-	 	_,
-	 	Backbone,
-	 	HTMLContentModel){
+    'underscore',
+    'backbone',
+    'goatApp/model/HTMLContentModel'],
+     function($,
+        _,
+        Backbone,
+        HTMLContentModel){
 
-	return HTMLContentModel.extend({
-		urlRoot:null,
-		defaults: {
-			items:null,
-			selectedItem:null
-		},
-		initialize: function (options) {
-			this.screenParam = null;
-			this.menuParam = null;
-			this.stageParam = null;
-			this.baseUrlRoot = 'attack?Screen=';
-		},
-		loadData: function(options) {
-			this.urlRoot = this.baseUrlRoot  +options.screen + '&menu=' + options.menu + '&stage=' + options.stage;
-			this.set('menuParam',options.menu);
-			this.set('screenParam',options.screen);
-			this.set('stageParam',options.stage)
-			var self=this;
-			this.fetch().then(function(data) {
-				self.setContent(data);
-			});
-		},
+    return HTMLContentModel.extend({
+        urlRoot:null,
+        defaults: {
+            items:null,
+            selectedItem:null
+        },
+        initialize: function (options) {
+            this.scrParam = null;
+            this.menuParam = null;
+            this.stageParam = null;
+            this.numParam = null;
+            this.baseUrlRoot = 'attack';
+        },
+        loadData: function(options) {
+            this.urlRoot = this.baseUrlRoot + "?Screen=" + options.scr + '&menu=' + options.menu;
+            if (options.stage != null) {
+               this.urlRoot += '&stage=' + options.stage;
+            }
+            if (options.num != null) {
+               this.urlRoot += '&Num=' + options.num;
+            }
+            this.set('menuParam', options.menu);
+            this.set('scrParam', options.scr);
+            this.set('stageParam', options.stage)
+            this.set('numParam', options.num)
+            var self = this;
+            this.fetch().done(function(data) {
+                self.setContent(data);
+            });
+        },
 
-		setContent: function(content, loadHelps) {
-			if (typeof loadHelps === 'undefined') {
-				loadHelps = true;
-			}
-			this.set('content',content);
-			this.trigger('content:loaded',this,loadHelps);
-		},
+        setContent: function(content, loadHelps) {
+            if (typeof loadHelps === 'undefined') {
+                loadHelps = true;
+            }
+            this.set('content',content);
+            this.trigger('content:loaded',this,loadHelps);
+        },
 
-		fetch: function (options) {
-			options = options || {};
-			return Backbone.Model.prototype.fetch.call(this, _.extend({ dataType: "html"}, options));
-		}
-	});
+        fetch: function (options) {
+            options = options || {};
+            return Backbone.Model.prototype.fetch.call(this, _.extend({ dataType: "html"}, options));
+        }
+    });
 });
--- webgoat-container/src/main/webapp/js/goatApp/view/LessonContentView.js
+++ webgoat-container/src/main/webapp/js/goatApp/view/LessonContentView.js
@@ -1,59 +1,64 @@
 //LessonContentView
 define(['jquery',
-	'underscore',
-	'backbone',
-	'libs/jquery.form'], 
-	function(
-		$,
-		_,
-		Backbone,
-		JQueryForm) {
-	return Backbone.View.extend({
-		el:'#lesson-content-wrapper', //TODO << get this fixed up in DOM
+    'underscore',
+    'backbone',
+    'libs/jquery.form'], 
+    function(
+        $,
+        _,
+        Backbone,
+        JQueryForm) {
+    return Backbone.View.extend({
+        el:'#lesson-content-wrapper', //TODO << get this fixed up in DOM
 
-		initialize: function(options) {
-			options = options || {};
-		},
+        initialize: function(options) {
+            options = options || {};
+        },
 
-		render: function() {
-			this.$el.html(this.model.get('content'));
-			this.makeFormsAjax();
-			this.ajaxifyAttackHref();
-			$(window).scrollTop(0); //work-around til we get the scroll down sorted out
-		},
-		
-		//TODO: reimplement this in custom fashion maybe?
-		makeFormsAjax: function () {
-			var options = {
-			    success:this.reLoadView.bind(this),
-			    url:'attack?Screen=' + this.model.get('screenParam') + '&menu=' + this.model.get('menuParam'),
-			    type:'GET'
-	            // $.ajax options can be used here too, for example: 
-	            //timeout:   3000 
-			};
-			//hook forms //TODO: clarify form selectors later
-		    $("form").ajaxForm(options);
+        render: function() {
+            this.$el.html(this.model.get('content'));
+            this.makeFormsAjax();
+            this.ajaxifyAttackHref();
+            $(window).scrollTop(0); //work-around til we get the scroll down sorted out
+        },
+        
+        //TODO: reimplement this in custom fashion maybe?
+        makeFormsAjax: function () {
+            var options = {
+                success:this.reLoadView.bind(this),
+                url: this.model.urlRoot,
+                type:'GET'
+                // $.ajax options can be used here too, for example: 
+                //timeout:   3000 
+            };
+            //hook forms //TODO: clarify form selectors later
+            $("form").ajaxForm(options);
         },
 
         ajaxifyAttackHref: function() {  // rewrite any links with hrefs point to relative attack URLs             
-        var self = this;
-        	$.each($('a[href^="attack?"]'),function(i,el) {
-        		var url = $(el).attr('href');
-        		$(el).unbind('click').attr('href','#').attr('link',url);
-        		//TODO pull currentMenuId
-        		$(el).click(function() {
-        			event.preventDefault();
-        			var _url = $(el).attr('link');
-        			$.get(_url, {success:self.reloadView.bind(self)});
-        		});
-        	});
-		},
+            var self = this;
+            // The current LessonAdapter#getLink() generates a hash-mark link.  It will not match the mask below.
+            // Besides, the new MVC code registers an event handler that will reload the lesson according to the route.
+            $.each($('a[href^="attack?"]'),function(i,el) {
+                var url = $(el).attr('href');
+                $(el).unbind('click').attr('href','#').attr('link',url);
+                //TODO pull currentMenuId
+                $(el).click(function(event) {
+                    event.preventDefault();
+                    var _url = $(el).attr('link');
+                    console.log("About to GET " + _url);
+                    $.get(_url)
+                        .done(self.reLoadView.bind(self))
+                        .fail(function() { alert("failed to GET " + _url); });
+                });
+            });
+        },
 
         reLoadView: function(content) {
-        	this.model.setContent(content);
-        	this.render();
+            this.model.setContent(content);
+            this.render();
         }
-	});
+    });
 
-	
+    
 });
