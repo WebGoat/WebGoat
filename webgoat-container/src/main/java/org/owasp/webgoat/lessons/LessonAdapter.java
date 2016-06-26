7e08d8e8c1ca8348dfc0ccc41706010f217cc698
= <=>
2016-06-26 19:55:45 -0400

    Fixed parenthesis that were out of place
    Build was failing - parenthesis was out of place. Fixed now.

--- webgoat-container/src/main/java/org/owasp/webgoat/lessons/LessonAdapter.java
+++ webgoat-container/src/main/java/org/owasp/webgoat/lessons/LessonAdapter.java
@@ -73,12 +73,12 @@ public abstract class LessonAdapter extends AbstractLesson {
                                 + "In fact, most lessons can be created by following the easy to use instructions by going to the WebGoat wiki page&nbsp;"
                                 + "<A HREF=https://github.com/WebGoat/WebGoat/wiki>WebGoat Wiki Page</A>&nbsp;&nbsp;"
                                 + "If you would prefer, send your lesson ideas to&nbsp;"
-                                + getWebgoatContext().getFeedbackAddressHTML())
+                                + getWebgoatContext().getFeedbackAddressHTML()  
                                 + " Note: you will need to register at 
                                 + "the following link to use the feedback tool:&nbsp;"
                                 + "<A HREF=https://lists.owasp.org/mailman/listinfo/owasp-webgoat>List Registration</A>&nbsp;&nbsp;"
                                 + "Finally, OWASP has a slack channel. You can register at the following link:&nbsp;"
-                                + "<A HREF=https://owasp.slack.com/>OWASP Slack Channel</A>");
+                                + "<A HREF=https://owasp.slack.com/>OWASP Slack Channel</A>"));
         
         try (InputStream is = Thread.currentThread().getContextClassLoader()
                 .getResourceAsStream("New Lesson Instructions.txt")) {
