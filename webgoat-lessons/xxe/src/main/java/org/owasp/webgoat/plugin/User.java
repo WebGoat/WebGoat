f698a2d6ae177ac617affe04478c0c5da47f4234
Nanne Baars <nbaars@xebia.com>
2016-11-17 16:27:41 +0100

    XXE first attempt

---
+++ webgoat-lessons/xxe/src/main/java/org/owasp/webgoat/plugin/User.java
@@ -0,0 +1,65 @@
+package org.owasp.webgoat.plugin;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * ************************************************************************************************
+ * This file is part of WebGoat, an Open Web Application Security Project utility. For details,
+ * please see http://www.owasp.org/
+ * <p>
+ * Copyright (c) 2002 - 20014 Bruce Mayhew
+ * <p>
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ * <p>
+ * Getting Source ==============
+ * <p>
+ * Source for this application is maintained at https://github.com/WebGoat/WebGoat, a repository for free software
+ * projects.
+ * <p>
+ *
+ * @author nbaars
+ * @version $Id: $Id
+ * @since November 17, 2016
+ */
+@XmlRootElement
+public class User {
+
+    private String username = "";
+    private String password = "";
+    private String email = "";
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
---
+++ webgoat-lessons/xxe/src/main/resources/plugin/XXE/js/xxe.js
@@ -0,0 +1,15 @@
+webgoat.customjs.register = function () {
+    var xml = '<?xml version="1.0"?>' +
+        '<user>' +
+        '  <username>' + 'test' + '</username>' +
+        '  <password>' + 'test' + '</password>' +
+        '</user>';
+    return xml;
+}
+webgoat.customjs.registerJson = function () {
+    var json = '{' +
+        '  "user":' + '"test"' +
+        '  "password":' + '"test"' +
+        '}';
+    return json;
+}
