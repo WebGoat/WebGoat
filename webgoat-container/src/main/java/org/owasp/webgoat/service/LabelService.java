2b0b49c0c268b5a47c8235c5e024856704ebf6c3
Mario Zupan <mzupanmz@gmail.com>
2016-11-23 15:36:02 +0100

    Remove unused import

--- webgoat-container/src/main/java/org/owasp/webgoat/service/LabelService.java
+++ webgoat-container/src/main/java/org/owasp/webgoat/service/LabelService.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
