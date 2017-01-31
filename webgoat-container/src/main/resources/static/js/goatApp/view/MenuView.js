b4106919d06941cbf82638a8abe40c5eaaf9a6f9
Jason White <jason.white@owasp.org>
2017-01-31 14:47:35 -0500

    cleaning up, fixing selected lesson class/es

--- webgoat-container/src/main/resources/static/js/goatApp/view/MenuView.js
+++ webgoat-container/src/main/resources/static/js/goatApp/view/MenuView.js
@@ -108,14 +108,15 @@ define(['jquery',
 		},
 
 		onLessonClick: function (elementId) {
-			$('#'+this.curLessonLinkId).removeClass('selected');
+			$('#'+this.curLessonLinkId).removeClass('selected').parent().removeClass('selected');
 			//update
-			$('#'+elementId).addClass('selected');
+			$('#'+elementId).addClass('selected').parent().addClass('selected');
 			this.curLessonLinkId = elementId;
 		},
 
 		expandCategory: function (id) {
 			if (id) {
+			    //this.selectedCategory = id;
 				this.accordionMenu(id);
 			}
 		},
