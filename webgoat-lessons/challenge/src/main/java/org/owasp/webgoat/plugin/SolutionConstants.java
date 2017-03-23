debc1e4b737ead176267abe6ed0e19585921c5cb
Nanne Baars <nanne.baars@owasp.org>
2017-03-23 16:12:04 +0100

    Added more for challenge 1 including input form group for flag

---
+++ webgoat-lessons/challenge/src/main/java/org/owasp/webgoat/plugin/SolutionConstants.java
@@ -0,0 +1,13 @@
+package org.owasp.webgoat.plugin;
+
+/**
+ * Interface with constants so we can easily change the flags
+ *
+ * @author nbaars
+ * @since 3/23/17.
+ */
+public interface SolutionConstants {
+
+    String PASSWORD = "!!webgoat_admin_1234!!";
+
+}
