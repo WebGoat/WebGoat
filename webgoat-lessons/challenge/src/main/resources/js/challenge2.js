599f36fdb8a7a44bae38f8e72d5754f0476a66de
Nanne Baars <nanne.baars@owasp.org>
2017-04-08 10:31:56 +0200

    Challenge 2 JavaScript is working

--- webgoat-lessons/challenge/src/main/resources/js/challenge2.js
+++ webgoat-lessons/challenge/src/main/resources/js/challenge2.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //-- Click on detail
-    $("ul.menu-items > li").on("click",function(){
+    $("ul.menu-items > li").on("click", function () {
         $("ul.menu-items > li").removeClass("active");
         $(this).addClass("active");
     })
 
-    $(".attr,.attr2").on("click",function(){
+    $(".attr,.attr2").on("click", function () {
         var clase = $(this).attr("class");
 
         $("." + clase).removeClass("active");
@@ -13,21 +13,36 @@ $(document).ready(function(){
     })
 
     //-- Click on QUANTITY
-    $(".btn-minus").on("click",function(){
+    $(".btn-minus").on("click", function () {
         var now = $(".section > div > input").val();
-        if ($.isNumeric(now)){
-            if (parseInt(now) -1 > 0){ now--;}
-            $(".section > div > input").val(now);
-        }else{
-            $(".section > div > input").val("1");
+        if ($.isNumeric(now)) {
+            if (parseInt(now) - 1 > 0) {
+                now--;
+            }
+            $(".quantity").val(now);
+        } else {
+            $(".quantity").val("1");
         }
     })
-    $(".btn-plus").on("click",function(){
+    $(".btn-plus").on("click", function () {
         var now = $(".section > div > input").val();
-        if ($.isNumeric(now)){
-            $(".section > div > input").val(parseInt(now)+1);
-        }else{
-            $(".section > div > input").val("1");
+        if ($.isNumeric(now)) {
+            $(".quantity").val(parseInt(now) + 1);
+        } else {
+            $(".quantity").val("1");
         }
+    })
+    $(".checkoutCode").on("blur", function () {
+        var checkoutCode = $(".checkoutCode").val();
+        $.get("challenge-store/coupons/" + checkoutCode, function (result, status) {
+            var discount = result.discount;
+            if (discount > 0) {
+                var price = $('#price').val();
+                $('#price').text((899 - (899 * discount / 100)).toFixed(2));
+            } else {
+                $('#price').text(899);
+            }
+
+        });
     })
 })
