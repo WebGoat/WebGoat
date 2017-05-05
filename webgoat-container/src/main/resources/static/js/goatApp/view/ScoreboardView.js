480dfe6a0a89350cc909ad23471117e1d74f9177
Jason White <jason.white@owasp.org>
2017-05-05 13:12:01 -0400

    scoreboard v 0.1 we'll say

--- webgoat-container/src/main/resources/static/js/goatApp/view/ScoreboardView.js
+++ webgoat-container/src/main/resources/static/js/goatApp/view/ScoreboardView.js
@@ -12,16 +12,16 @@ function($,
 		el:'#scoreboard',
 
 		initialize: function() {
-		    template:ScoreboardTemplate,
-			this.collection = new FlagsCollection();
-			this.listenTo(this.collection,'reset',this.render)
-			this.collection.fetch({reset:true});
+		    this.template = ScoreboardTemplate,
+		    this.collection = new FlagsCollection();
+		    this.listenTo(this.collection,'reset',this.render)
+		    this.collection.fetch({reset:true});
 		},
 
 		render: function() {
 			//this.$el.html('test');
 			var t = _.template(this.template);
-            this.$el.html(t({'flags':this.collection.toJSON()}));
+            this.$el.html(t({'rankings':this.collection.toJSON()}));
 			//TODO: add template (table) to iterate over ...
 			//this.collection.toJSON(); << put that in the template data
 			//TODO: set up next poll here with listenToOnce
