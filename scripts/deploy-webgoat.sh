904a6b363d04bfa17cab1c21aabf0c6ee7eb9da6
Nanne Baars <nanne.baars@owasp.org>
2017-10-18 19:52:57 +0200

    Fixing Docker build

--- scripts/deploy-webgoat.sh
+++ scripts/deploy-webgoat.sh
@@ -7,13 +7,11 @@ cd webgoat-server
 
 if [ "${BRANCH}" == "master" ] && [ ! -z "${TRAVIS_TAG}" ]; then
   # If we push a tag to master this will update the LATEST Docker image and tag with the version number
-  docker build -f Dockerfile -t $REPO:latest .
-  docker tag $REPO:${TRAVIS_TAG}
+  docker build -f Dockerfile -t $REPO:latest -t $REPO:${TRAVIS_TAG} .
   docker push $REPO
 elif [ ! -z "${TRAVIS_TAG}" ]; then
   # Creating a tag build we push it to Docker with that tag
   docker build -f Dockerfile -t $REPO:${TRAVIS_TAG} .
-  docker tag $REPO:${TRAVIS_TAG}
   docker push $REPO
 elif [ "${BRANCH}" == "develop" ]; then
   docker build -f Dockerfile -t $REPO:snapshot .
