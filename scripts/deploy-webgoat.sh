044bcd995611fa3eb0681c2fbf11fce98b3fc031
Nanne Baars <nanne.baars@owasp.org>
2018-04-11 20:22:19 +0200

    Wrong image name mentioned in lesson for WebWolf

--- scripts/deploy-webgoat.sh
+++ scripts/deploy-webgoat.sh
@@ -20,3 +20,21 @@ elif [ ! -z "${TRAVIS_TAG}" ]; then
 else
   echo "Skipping releasing to DockerHub because it is a build of branch ${BRANCH}"
 fi
+
+
+export REPO=webgoat/webwolf
+cd ..
+cd webwolf
+ls target/
+
+if [ "${BRANCH}" == "master" ] && [ ! -z "${TRAVIS_TAG}" ]; then
+  # If we push a tag to master this will update the LATEST Docker image and tag with the version number
+  docker build --build-arg webwolf_version=${TRAVIS_TAG:1} -f Dockerfile -t $REPO:latest -t $REPO:${TRAVIS_TAG} .
+  docker push $REPO
+elif [ ! -z "${TRAVIS_TAG}" ]; then
+  # Creating a tag build we push it to Docker with that tag
+  docker build --build-arg webwolf_version=${TRAVIS_TAG:1} -f Dockerfile -t $REPO:${TRAVIS_TAG} -t $REPO:latest .
+  docker push $REPO
+else
+  echo "Skipping releasing to DockerHub because it is a build of branch ${BRANCH}"
+fi
