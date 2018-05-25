33f1c86ae68c053719da91a0b3762ee12d49056c
Nanne Baars <nanne.baars@owasp.org>
2018-05-25 14:27:45 +0200

    Initial commit for password reset lesson

---
+++ webgoat-lessons/password-reset/src/main/resources/js/password-reset-simple.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#olvidado').click(function(e) {
+        e.preventDefault();
+        $('div#form-olvidado').toggle('500');
+    });
+    $('#acceso').click(function(e) {
+        e.preventDefault();
+        $('div#form-olvidado').toggle('500');
+    });
+});
