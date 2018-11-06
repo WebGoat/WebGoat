d29b80de255f347c2d7be5fab91332fc8a60c723
Benedikt - Desktop <benedikt.stuhrmann@gmail.com>
2018-11-06 17:44:26 +0100

    Reworked and polished assignment 8 and 9 (C and I)

--- webgoat-container/src/main/java/org/owasp/webgoat/session/CreateDB.java
+++ webgoat-container/src/main/java/org/owasp/webgoat/session/CreateDB.java
@@ -1007,7 +1007,7 @@ public class CreateDB {
                     + "first_name varchar(20),"
                     + "last_name varchar(20),"
                     + "department varchar(20),"
-                    + "salary varchar(10),"
+                    + "salary int,"
                     + "auth_tan varchar(6)"
                     + ")";
             statement.executeUpdate(createTableStatement);
@@ -1016,11 +1016,11 @@ public class CreateDB {
         }
 
         // Populate
-        String insertData1 = "INSERT INTO employees VALUES ('32147','Paulina',  'Travers', 'Accounting',  '$46.000', 'P45JSI')";
-        String insertData2 = "INSERT INTO employees VALUES ('89762','Tobi',     'Barnett', 'Development', '$77.000', 'TA9LL1')";
-        String insertData3 = "INSERT INTO employees VALUES ('96134','Bob',      'Franco',  'Marketing',   '$83.700', 'LO9S2V')";
-        String insertData4 = "INSERT INTO employees VALUES ('34477','Abraham ', 'Holman',  'Development', '$50.000', 'UU2ALK')";
-        String insertData5 = "INSERT INTO employees VALUES ('37648','John',     'Smith',   'Marketing',   '$64.350', '3SL99A')";
+        String insertData1 = "INSERT INTO employees VALUES ('32147','Paulina',  'Travers', 'Accounting',  46000, 'P45JSI')";
+        String insertData2 = "INSERT INTO employees VALUES ('89762','Tobi',     'Barnett', 'Development', 77000, 'TA9LL1')";
+        String insertData3 = "INSERT INTO employees VALUES ('96134','Bob',      'Franco',  'Marketing',   83700, 'LO9S2V')";
+        String insertData4 = "INSERT INTO employees VALUES ('34477','Abraham ', 'Holman',  'Development', 50000, 'UU2ALK')";
+        String insertData5 = "INSERT INTO employees VALUES ('37648','John',     'Smith',   'Marketing',   64350, '3SL99A')";
         statement.executeUpdate(insertData1);
         statement.executeUpdate(insertData2);
         statement.executeUpdate(insertData3);
