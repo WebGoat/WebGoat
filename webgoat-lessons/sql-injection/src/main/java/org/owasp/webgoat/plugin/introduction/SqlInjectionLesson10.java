7602c9a8379e2098b8db9f6e8706fabcbd67b94b
Benedikt - Desktop <benedikt.stuhrmann@gmail.com>
2018-11-06 11:21:25 +0100

    Added more hints

--- webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/plugin/introduction/SqlInjectionLesson10.java
+++ webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/plugin/introduction/SqlInjectionLesson10.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.sql.*;
 
 @AssignmentPath("/SqlInjection/attack10")
+@AssignmentHints(value = {"SqlStringInjectionHint10-1", "SqlStringInjectionHint10-2", "SqlStringInjectionHint10-3", "SqlStringInjectionHint10-4", "SqlStringInjectionHint10-5", "SqlStringInjectionHint10-6"})
 public class SqlInjectionLesson10 extends AssignmentEndpoint {
 
     @RequestMapping(method = RequestMethod.POST)
