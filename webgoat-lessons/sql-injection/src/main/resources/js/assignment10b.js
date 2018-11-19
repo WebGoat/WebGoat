8a7436cb9d9fdadbf9f08cc4f84c0da1bc424b30
Max Geldner <max.sgeldner@t-online.de>
2018-11-19 17:48:49 +0100

    Added comment parsing and feedback to text editor lessons

--- webgoat-lessons/sql-injection/src/main/resources/js/assignment10b.js
+++ webgoat-lessons/sql-injection/src/main/resources/js/assignment10b.js
@@ -1,16 +1,25 @@
 function ace_collect() {
     let code = "";
+    console.log("Test");
     $(".ace_line").each(function(i, el) {
-        code += el.innerHTML;
+        var to_add = el.innerHTML;
+        if(/\/\/.*/.test(to_add)) {
+            to_add = to_add.replace(/\/\/.*/i, '');
+        }
+        code += to_add;
     });
-    console.log(code);
-    code = $(".ace_content")[0].innerHTML;
     $.ajax({
         type: "POST",
         url: "/WebGoat/SqlInjection/attack10b",
         dataType: "text",
         data: {
             editor: code
+        },
+        success: function(data) {
+            console.log("entry");
+            let lesson_feedback = JSON.parse(data);
+            $("#insertcode .attack-feedback").css("display", "block");
+            $("#insertcode .attack-feedback").html(lesson_feedback.feedback);
         }
     });
 }
