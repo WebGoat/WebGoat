09baecb3e965e287efde6fdb891678ab40cbdf36
Tobias_Melzer <Tobias_Melzer@t-online.de>
2018-11-27 02:44:06 +0100

    Added second assignment for chrome developer tools.

---
+++ webgoat-lessons/chrome-dev-tools/src/main/java/org/owasp/webgoat/plugin/NetworkLesson.java
@@ -0,0 +1,26 @@
+package org.owasp.webgoat.plugin;
+
+import org.owasp.webgoat.assignments.AssignmentEndpoint;
+import org.owasp.webgoat.assignments.AssignmentPath;
+import org.owasp.webgoat.assignments.AttackResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+
+@AssignmentPath("/ChromeDevTools/network")
+public class NetworkLesson extends AssignmentEndpoint {
+
+  @RequestMapping(method = RequestMethod.POST)
+  public
+  @ResponseBody
+  AttackResult completed(@RequestParam String network_num, @RequestParam String number) throws IOException {
+    if(network_num.equals(number)) {
+      return trackProgress(success().feedback("network.success").build());
+    } else {
+      return trackProgress(failed().feedback("network.failed").build());
+    }
+  }
+}
