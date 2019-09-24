a4501870d48bc6cfbe1eb5ca7ebe39325498dcb2
Nanne Baars <nanne.baars@owasp.org>
2019-09-24 22:41:20 +0200

    SQL: Cannot use apostrophe/quotes on string literals #662

--- webgoat-lessons/sql-injection/src/test/java/org/owasp/webgoat/sql_injection/introduction/SqlInjectionLesson5aTest.java
+++ webgoat-lessons/sql-injection/src/test/java/org/owasp/webgoat/sql_injection/introduction/SqlInjectionLesson5aTest.java
@@ -1,22 +1,17 @@
 package org.owasp.webgoat.sql_injection.introduction;
 
-import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.owasp.webgoat.plugins.LessonTest;
-import org.owasp.webgoat.session.WebgoatContext;
 import org.owasp.webgoat.sql_injection.SqlLessonTest;
-import org.owasp.webgoat.sql_injection.introduction.SqlInjection;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
+
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,65 +22,50 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 public class SqlInjectionLesson5aTest extends SqlLessonTest {
 
-  @Test
-  public void knownAccountShouldDisplayData() throws Exception {
-    LinkedMultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-    map.add("account", "Smith");
-    map.add("operator", "");
-    map.add("injection", "");
-    mockMvc.perform(MockMvcRequestBuilders.post("/SqlInjection/assignment5a")
-            .params(map))
+    @Test
+    public void knownAccountShouldDisplayData() throws Exception {
+        var params = Map.of("account", "Smith", "operator", "", "injection", "");
+        mockMvc.perform(MockMvcRequestBuilders.post("/SqlInjection/assignment5a")
+                .params(new LinkedMultiValueMap(params)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("lessonCompleted", is(false)))
+                .andExpect(jsonPath("$.feedback", is(messages.getMessage("assignment.not.solved"))))
+                .andExpect(jsonPath("$.output", containsString("<p>USERID, FIRST_NAME")));
+    }
 
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("lessonCompleted", is(false)))
-            .andExpect(jsonPath("$.feedback", is(messages.getMessage("assignment.not.solved"))))
-            .andExpect(jsonPath("$.output", containsString("<p>USERID, FIRST_NAME")));
-  }
+    @Ignore
+    @Test
+    public void unknownAccount() throws Exception {
+        var params = Map.of("account", "Smith", "operator", "", "injection", "");
+        mockMvc.perform(MockMvcRequestBuilders.post("/SqlInjection/assignment5a")
+                .params(new LinkedMultiValueMap(params)))
 
-  @Ignore
-  @Test
-  public void unknownAccount() throws Exception {
-    LinkedMultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-    map.add("account", "Smith");
-    map.add("operator", "");
-    map.add("injection", "");
-    mockMvc.perform(MockMvcRequestBuilders.post("/SqlInjection/assignment5a")
-            .params(map))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("lessonCompleted", is(false)))
+                .andExpect(jsonPath("$.feedback", is(SqlInjectionLesson8Test.modifySpan(messages.getMessage("NoResultsMatched")))))
+                .andExpect(jsonPath("$.output").doesNotExist());
+    }
 
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("lessonCompleted", is(false)))
-            .andExpect(jsonPath("$.feedback", is(SqlInjectionLesson8Test.modifySpan(messages.getMessage("NoResultsMatched")))))
-            .andExpect(jsonPath("$.output").doesNotExist());
-  }
+    @Test
+    public void sqlInjection() throws Exception {
+        var params = Map.of("account", "'", "operator", "OR", "injection", "'1' = '1");
+        mockMvc.perform(MockMvcRequestBuilders.post("/SqlInjection/assignment5a")
+                .params(new LinkedMultiValueMap(params)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("lessonCompleted", is(true)))
+                .andExpect(jsonPath("$.feedback", containsString("You have succeed")))
+                .andExpect(jsonPath("$.output").exists());
+    }
 
-  @Test
-  public void sqlInjection() throws Exception {
-    LinkedMultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-    map.add("account", "'");
-    map.add("operator", "OR");
-    map.add("injection", "'1' = '1");
-    mockMvc.perform(MockMvcRequestBuilders.post("/SqlInjection/assignment5a")
-            .params(map))
-
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("lessonCompleted", is(true)))
-            .andExpect(jsonPath("$.feedback", containsString("You have succeed")))
-            .andExpect(jsonPath("$.output").exists());
-  }
-
-  @Test
-  public void sqlInjectionWrongShouldDisplayError() throws Exception {
-    LinkedMultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-    map.add("account", "Smith'");
-    map.add("operator", "OR");
-    map.add("injection", "'1' = '1'");
-    mockMvc.perform(MockMvcRequestBuilders.post("/SqlInjection/assignment5a")
-            .params(map))
-
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("lessonCompleted", is(false)))
-            .andExpect(jsonPath("$.feedback", containsString(messages.getMessage("assignment.not.solved"))))
-            .andExpect(jsonPath("$.output", is("malformed string: '1''<br> Your query was: SELECT * FROM user_data WHERE" +
-                    " first_name = 'John' and last_name = 'Smith' OR '1' = '1''")));
-  }
+    @Test
+    public void sqlInjectionWrongShouldDisplayError() throws Exception {
+        var params = Map.of("account", "Smith'", "operator", "OR", "injection", "'1' = '1'");
+        mockMvc.perform(MockMvcRequestBuilders.post("/SqlInjection/assignment5a")
+                .params(new LinkedMultiValueMap(params)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("lessonCompleted", is(false)))
+                .andExpect(jsonPath("$.feedback", containsString(messages.getMessage("assignment.not.solved"))))
+                .andExpect(jsonPath("$.output", is("malformed string: '1''<br> Your query was: SELECT * FROM user_data WHERE" +
+                        " first_name = 'John' and last_name = 'Smith' OR '1' = '1''")));
+    }
 }
