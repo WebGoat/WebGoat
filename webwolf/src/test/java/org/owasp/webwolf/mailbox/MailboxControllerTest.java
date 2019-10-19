2ef06a601217a4c6cb587e3c70f23e369c43ee26
Nanne Baars <nanne.baars@owasp.org>
2019-10-19 19:19:22 +0200

    Final changes for splitting SQL WebGoat and lessons

--- webwolf/src/test/java/org/owasp/webwolf/mailbox/MailboxControllerTest.java
+++ webwolf/src/test/java/org/owasp/webwolf/mailbox/MailboxControllerTest.java
@@ -35,6 +35,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -49,6 +50,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(MailboxController.class)
+@ActiveProfiles({"test", "webwolf"})
 public class MailboxControllerTest {
 
     @Autowired
