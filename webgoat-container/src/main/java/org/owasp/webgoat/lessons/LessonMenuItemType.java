ad5ab4ca2e816c4c61ae6065651d15f3c7a4b611
Maxim Masiutin <maxim@masiutin.com>
2021-02-18 20:06:11 +0200

    Fixes #321 (#935)
    Copyright year was "20014", replaced to "2014"
    Fixed the old github.io URL which no longer exist
    See https://github.com/WebGoat/WebGoat/issues/321

--- webgoat-container/src/main/java/org/owasp/webgoat/lessons/LessonMenuItemType.java
+++ webgoat-container/src/main/java/org/owasp/webgoat/lessons/LessonMenuItemType.java
@@ -4,7 +4,7 @@
  * This file is part of WebGoat, an Open Web Application Security Project utility. For details,
  * please see http://www.owasp.org/
  * 
- * Copyright (c) 2002 - 20014 Bruce Mayhew
+ * Copyright (c) 2002 - 2014 Bruce Mayhew
  * 
  * This program is free software; you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation; either version 2 of the
