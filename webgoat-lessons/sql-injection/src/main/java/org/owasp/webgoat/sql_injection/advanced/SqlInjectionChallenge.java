ca7405fdd15a6e65ee9288f6770ab8814feb0179
QwertyStack <l.cachinero.2018@alumnos.urjc.es>
2021-03-14 19:14:32 +0100

    Update SqlInjectionChallenge.java

--- webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/sql_injection/advanced/SqlInjectionChallenge.java
+++ webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/sql_injection/advanced/SqlInjectionChallenge.java
@@ -62,7 +62,7 @@ public class SqlInjectionChallenge extends AssignmentEndpoint {
             try (Connection connection = dataSource.getConnection()) {
                 String checkUserQuery = "select userid from sql_challenge_users where userid = '" + username_reg + "'";
                 Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(checkUserQuery);
+                ResultSet resultSet = statement.executeQuery(checkUserQuery); 
 
                 if (resultSet.next()) {
                     if (username_reg.contains("tom'")) {
