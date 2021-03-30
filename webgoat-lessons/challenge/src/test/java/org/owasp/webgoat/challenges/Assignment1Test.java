14621003d79335cbd047fca71446929df2466901
Nanne Baars <nanne.baars@owasp.org>
2021-03-30 17:52:36 +0200

    Remove banner.txt as you can set it with a property in application.properties

--- webgoat-lessons/challenge/src/test/java/org/owasp/webgoat/challenges/Assignment1Test.java
+++ webgoat-lessons/challenge/src/test/java/org/owasp/webgoat/challenges/Assignment1Test.java
@@ -23,6 +23,7 @@
 package org.owasp.webgoat.challenges;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
