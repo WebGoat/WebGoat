1e0f170cf56629b676ef73c9ee628000b1d40b4c
Nanne Baars <nanne.baars@owasp.org>
2021-03-31 23:05:27 +0200

    Update to latest versions

--- webgoat-container/src/main/java/org/owasp/webgoat/asciidoc/WebGoatVersionMacro.java
+++ webgoat-container/src/main/java/org/owasp/webgoat/asciidoc/WebGoatVersionMacro.java
@@ -1,17 +1,22 @@
 package org.owasp.webgoat.asciidoc;
 
-import org.asciidoctor.ast.AbstractBlock;
+import org.asciidoctor.ast.ContentNode;
 import org.asciidoctor.extension.InlineMacroProcessor;
+
 import java.util.Map;
 
 public class WebGoatVersionMacro extends InlineMacroProcessor {
+
+    public WebGoatVersionMacro(String macroName) {
+        super(macroName);
+    }
 
     public WebGoatVersionMacro(String macroName, Map<String, Object> config) {
         super(macroName, config);
     }
 
     @Override
-	public String process(AbstractBlock parent, String target, Map<String, Object> attributes) {
+	public String process(ContentNode contentNode, String target, Map<String, Object> attributes) {
         return EnvironmentExposure.getEnv().getProperty("webgoat.build.version");
     }
 }
