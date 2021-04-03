d2445d7f11a3a182dcf9ce5770cfc46aa6369979
Nanne Baars <nanne.baars@owasp.org>
2021-04-03 16:25:06 +0200

    Update Dockerfile

--- docker/start.sh
+++ docker/start.sh
@@ -3,10 +3,14 @@
 cd /home/webgoat 
 service nginx start
 sleep 1
+echo "Starting WebGoat..."
 java -Duser.home=/home/webgoat -Dfile.encoding=UTF-8 -jar webgoat.jar --webgoat.build.version=$1 --server.address=0.0.0.0  > webgoat.log &
 
 sleep 10
- 
+
+echo "Starting WebWolf..."
 java -Duser.home=/home/webgoat -Dfile.encoding=UTF-8 -jar webwolf.jar --webgoat.build.version=$1 --server.address=0.0.0.0 > webwolf.log &
+
+echo "Browse to http://localhost" to get started >> webgoat.log
 
 tail -300f webgoat.log
