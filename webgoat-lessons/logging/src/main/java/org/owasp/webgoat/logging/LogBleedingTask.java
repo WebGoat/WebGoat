676a44071efff1778b9117fc5887bc71a31b4640
Jeroen Willemsen <jwillemsen@xebia.com>
2021-10-05 22:25:23 +0200

    Sign off
    Signed-off-by: Jeroen Willemsen <jwillemsen@xebia.com>

--- webgoat-lessons/logging/src/main/java/org/owasp/webgoat/logging/LogBleedingTask.java
+++ webgoat-lessons/logging/src/main/java/org/owasp/webgoat/logging/LogBleedingTask.java
@@ -25,22 +25,30 @@ package org.owasp.webgoat.logging;
 import org.apache.logging.log4j.util.Strings;
 import org.owasp.webgoat.assignments.AssignmentEndpoint;
 import org.owasp.webgoat.assignments.AttackResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 import java.util.Base64;
 
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
-import java.util.logging.Logger;
 
 @RestController
 public class LogBleedingTask extends AssignmentEndpoint {
-    private static final Logger LOG
-            = Logger.getLogger(LogBleedingTask.class.getName());
-    private String password = Base64.getEncoder().encodeToString(UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8));
 
+    Logger log = LoggerFactory.getLogger(this.getClass().getName());
+    private String password;
+
+    @PostConstruct
+    public void generatePassword(){
+        password = UUID.randomUUID().toString();
+        log.info("Password for admin: {}", Base64.getEncoder().encodeToString(password.getBytes(StandardCharsets.UTF_8)));
+    }
 
     @PostMapping("/LogSpoofing/log-bleeding")
     @ResponseBody
