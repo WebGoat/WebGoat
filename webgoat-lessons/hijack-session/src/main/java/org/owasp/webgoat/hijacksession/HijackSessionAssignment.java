15c4dc4fcfca0757343effab6fcfaccdff9bd622
Àngel Ollé Blázquez <angel@olleb.com>
2021-10-09 18:50:21 +0200

    Hijack Session Lesson

---
+++ webgoat-lessons/hijack-session/src/main/java/org/owasp/webgoat/hijacksession/HijackSessionAssignment.java
@@ -0,0 +1,92 @@
+/*
+ * This file is part of WebGoat, an Open Web Application Security Project utility. For details, please see http://www.owasp.org/
+ *
+ * Copyright (c) 2002 - 2021 Bruce Mayhew
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Getting Source ==============
+ *
+ * Source for this application is maintained at https://github.com/WebGoat/WebGoat, a repository for free software projects.
+ */
+
+package org.owasp.webgoat.hijacksession;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.owasp.webgoat.assignments.AssignmentEndpoint;
+import org.owasp.webgoat.assignments.AssignmentHints;
+import org.owasp.webgoat.assignments.AttackResult;
+import org.owasp.webgoat.hijacksession.cas.Authentication;
+import org.owasp.webgoat.hijacksession.cas.HijackSessionAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/***
+ *
+ * @author Angel Olle Blazquez
+ *
+ */
+
+@RestController
+@AssignmentHints({
+    "hijacksession.hints.1",
+    "hijacksession.hints.2",
+    "hijacksession.hints.3",
+    "hijacksession.hints.4",
+    "hijacksession.hints.5"
+})
+public class HijackSessionAssignment extends AssignmentEndpoint {
+
+    private static final String COOKIE_NAME = "hijack_cookie";
+
+    @Autowired
+    HijackSessionAuthenticationProvider provider;
+
+    @PostMapping(path = "/HijackSession/login")
+    @ResponseBody
+    public AttackResult login(
+        @RequestParam String username,
+        @RequestParam String password,
+        @CookieValue(value = COOKIE_NAME, required = false) String cookieValue,
+        HttpServletResponse response) {
+
+        Authentication authentication;
+        if (StringUtils.isEmpty(cookieValue)) {
+            authentication = provider.authenticate(Authentication.builder().name(username).credentials(password).build());
+            setCookie(response, authentication.getId());
+        } else {
+            authentication = provider.authenticate(Authentication.builder().id(cookieValue).build());
+        }
+
+        if (authentication.isAuthenticated()) {
+            return success(this).build();
+        }
+
+        return failed(this).build();
+    }
+
+    private void setCookie(HttpServletResponse response, String cookieValue) {
+        Cookie cookie = new Cookie(COOKIE_NAME, cookieValue);
+        cookie.setPath("/WebGoat");
+        cookie.setSecure(true);
+        response.addCookie(cookie);
+    }
+
+}
---
+++ webgoat-lessons/hijack-session/src/test/java/org/owasp/webgoat/hijacksession/HijackSessionAssignmentTest.java
@@ -0,0 +1,108 @@
+/*
+ * This file is part of WebGoat, an Open Web Application Security Project utility. For details, please see http://www.owasp.org/
+ *
+ * Copyright (c) 2002 - 2021 Bruce Mayhew
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Getting Source ==============
+ *
+ * Source for this application is maintained at https://github.com/WebGoat/WebGoat, a repository for free software projects.
+ */
+
+package org.owasp.webgoat.hijacksession;
+
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.not;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import javax.servlet.http.Cookie;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.owasp.webgoat.assignments.AssignmentEndpointTest;
+import org.owasp.webgoat.hijacksession.cas.Authentication;
+import org.owasp.webgoat.hijacksession.cas.HijackSessionAuthenticationProvider;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+/***
+ *
+ * @author Angel Olle Blazquez
+ *
+ */
+
+@ExtendWith(MockitoExtension.class)
+class HijackSessionAssignmentTest extends AssignmentEndpointTest {
+
+    private MockMvc mockMvc;
+    private static final String COOKIE_NAME = "hijack_cookie";
+    private static final String LOGIN_CONTEXT_PATH = "/HijackSession/login";
+
+    @Mock
+    Authentication authenticationMock;
+
+    @Mock
+    HijackSessionAuthenticationProvider providerMock;
+
+    HijackSessionAssignment assignment;
+
+    @BeforeEach
+    void setup() {
+        assignment = new HijackSessionAssignment();
+        init(assignment);
+        ReflectionTestUtils.setField(assignment, "provider", new HijackSessionAuthenticationProvider());
+        mockMvc = standaloneSetup(assignment).build();
+    }
+
+    @Test
+    void testValidCookie() throws Exception {
+        lenient().when(authenticationMock.isAuthenticated()).thenReturn(true);
+        lenient().when(providerMock.authenticate(any(Authentication.class))).thenReturn(authenticationMock);
+        ReflectionTestUtils.setField(assignment, "provider", providerMock);
+
+        Cookie cookie = new Cookie(COOKIE_NAME, "value");
+
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders
+            .post(LOGIN_CONTEXT_PATH)
+            .cookie(cookie)
+            .param("username", "")
+            .param("password", ""));
+
+        result.andExpect(jsonPath("$.lessonCompleted", CoreMatchers.is(true)));
+
+    }
+
+    @Test
+    void testBlankCookie() throws Exception {
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders
+            .post(LOGIN_CONTEXT_PATH)
+            .param("username", "webgoat")
+            .param("password", "webgoat"));
+
+        result.andExpect(cookie().value(COOKIE_NAME, not(emptyString())));
+        result.andExpect(jsonPath("$.lessonCompleted", CoreMatchers.is(false)));
+
+    }
+
+}
