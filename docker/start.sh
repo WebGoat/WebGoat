b14f7df33a653733811174851f41eaa95aa1e600
Nanne Baars <nanne.baars@owasp.org>
2021-10-23 20:57:16 +0200

    Move to different base image for Java This way we can also support arm/v7

--- docker/start.sh
+++ docker/start.sh
@@ -16,11 +16,11 @@ java \
  --add-opens java.desktop/java.awt.font=ALL-UNNAMED \
  --add-opens java.base/sun.nio.ch=ALL-UNNAMED \
  --add-opens java.base/java.io=ALL-UNNAMED \
- -jar webgoat.jar --webgoat.build.version="$1" --server.address=0.0.0.0 > webgoat.log &
+ -jar webgoat.jar --server.address=0.0.0.0 > webgoat.log &
 
 echo "Starting WebWolf..."
-java -Duser.home=/home/webgoat -Dfile.encoding=UTF-8 -jar webwolf.jar --webgoat.build.version=$1 --server.address=0.0.0.0 > webwolf.log &
+java -Duser.home=/home/webgoat -Dfile.encoding=UTF-8 -jar webwolf.jar --server.address=0.0.0.0 > webwolf.log &
 
 echo "Browse to http://localhost to get started" >> webgoat.log
 
-tail -300f webgoat.log
+exec tail -300f webgoat.log
