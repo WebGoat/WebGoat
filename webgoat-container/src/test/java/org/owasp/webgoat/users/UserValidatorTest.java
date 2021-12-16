d721138d73d45b1bb9e0ef3984613a5db353fae3
Zubcevic.com <rene@zubcevic.com>
2021-12-16 15:41:39 +0100

    upgrade to latest spring-boot libs and fixed related issues

--- webgoat-container/src/test/java/org/owasp/webgoat/users/UserValidatorTest.java
+++ webgoat-container/src/test/java/org/owasp/webgoat/users/UserValidatorTest.java
@@ -9,7 +9,7 @@ import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
