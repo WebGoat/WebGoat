21c87707cca3bddef64378db1f41aa29d2d0b24c
Nanne Baars <nanne.baars@owasp.org>
2022-01-13 20:09:43 +0100

    Lesson scanner not working when fat jar
    When running the fat jar we have to take into account we
    are reading from the jar file and not the filesystem. In
    this case you cannot use `getFile` for example.

--- src/main/java/org/owasp/webgoat/container/lessons/LessonScanner.java
+++ src/main/java/org/owasp/webgoat/container/lessons/LessonScanner.java
@@ -2,31 +2,42 @@ package org.owasp.webgoat.container.lessons;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 @Component
 @Slf4j
 public class LessonScanner {
 
+    private static final Pattern lessonPattern = Pattern.compile("^.*/lessons/([^/]*)/.*$");
+
     @Getter
-    private List<String> lessons = new ArrayList<>();
+    private final Set<String> lessons = new HashSet<>();
 
     public LessonScanner(ResourcePatternResolver resourcePatternResolver) {
         try {
-            var resources = resourcePatternResolver.getResources("classpath:/lessons/*");
+            var resources = resourcePatternResolver.getResources("classpath:/lessons/*/*");
             for (var resource : resources) {
-                lessons.add(resource.getFilename());
+                //WG can run as a fat jar or as directly from file system we need to support both so use the URL
+                var url = resource.getURL();
+                var matcher = lessonPattern.matcher(url.toString());
+                if (matcher.matches()) {
+                    lessons.add(matcher.group(1));
+                }
             }
+            log.debug("Found {} lessons", lessons.size());
         } catch (IOException e) {
             log.warn("No lessons found...");
         }
+
     }
 
     public List<String> applyPattern(String pattern) {
