22b6f9d18bbbee3169583fd73bfd1c78a9856aa0
PabloMoreno24 <55099376+PabloMoreno24@users.noreply.github.com>
2022-04-05 11:30:43 +0200

    Update WebSecurityConfig.java
    Password Encoder

--- webgoat-container/src/main/java/org/owasp/webgoat/WebSecurityConfig.java
+++ webgoat-container/src/main/java/org/owasp/webgoat/WebSecurityConfig.java
@@ -77,7 +77,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Bean
