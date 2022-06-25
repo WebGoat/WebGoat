a80b0e386e0b90707a7e6238e80c54667df26c25
kikedalle <80217135+kikedalle@users.noreply.github.com>
2022-06-25 20:01:16 +0200

    Bcrypt Password

--- src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
@@ -43,6 +43,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 /**
  * Security configuration for WebGoat.
