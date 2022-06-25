4528400053d0470f6df8a8f921a071f45c5116cf
kikedalle <80217135+kikedalle@users.noreply.github.com>
2022-06-25 20:15:42 +0200

    Update WebSecurityConfig.java

--- src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
@@ -42,8 +42,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 
 /**
