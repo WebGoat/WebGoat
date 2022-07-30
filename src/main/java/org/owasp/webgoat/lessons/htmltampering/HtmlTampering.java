25948306bd1230fb152aa3ee730d4dc2cf220143
Àngel Ollé Blázquez <angel@olleb.com>
2022-07-30 19:25:59 +0200

    Renamed to htmltampering

--- src/main/java/org/owasp/webgoat/lessons/htmltampering/HtmlTampering.java
+++ src/main/java/org/owasp/webgoat/lessons/htmltampering/HtmlTampering.java
@@ -1,4 +1,4 @@
-package org.owasp.webgoat.lessons.html_tampering;
+package org.owasp.webgoat.lessons.htmltampering;
 
 import org.owasp.webgoat.container.lessons.Category;
 import org.owasp.webgoat.container.lessons.Lesson;
