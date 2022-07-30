827a9d3467a5e41656e967060cc1f4fd12ea69ba
Àngel Ollé Blázquez <angel@olleb.com>
2022-07-30 22:28:32 +0200

    Renamed to securepasswords

--- src/main/java/org/owasp/webgoat/lessons/securepasswords/SecurePasswords.java
+++ src/main/java/org/owasp/webgoat/lessons/securepasswords/SecurePasswords.java
@@ -20,7 +20,7 @@
  * Source for this application is maintained at https://github.com/WebGoat/WebGoat, a repository for free software projects.
  */
 
-package org.owasp.webgoat.lessons.secure_passwords;
+package org.owasp.webgoat.lessons.securepasswords;
 
 import org.owasp.webgoat.container.lessons.Category;
 import org.owasp.webgoat.container.lessons.Lesson;
