d991c20a58c872770c9ca35750aaac26a00b985c
Àngel Ollé Blázquez <angel@olleb.com>
2022-07-30 19:25:59 +0200

    Renamed to htmltampering

--- src/main/java/org/owasp/webgoat/lessons/htmltampering/HtmlTamperingTask.java
+++ src/main/java/org/owasp/webgoat/lessons/htmltampering/HtmlTamperingTask.java
@@ -20,7 +20,7 @@
  * Source for this application is maintained at https://github.com/WebGoat/WebGoat, a repository for free software projects.
  */
 
-package org.owasp.webgoat.lessons.html_tampering;
+package org.owasp.webgoat.lessons.htmltampering;
 
 import org.owasp.webgoat.container.assignments.AssignmentEndpoint;
 import org.owasp.webgoat.container.assignments.AssignmentHints;
