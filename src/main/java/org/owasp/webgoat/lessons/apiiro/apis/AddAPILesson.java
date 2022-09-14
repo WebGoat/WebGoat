c180ff0706144e668157a8d9adef8ebd7338976e
Idan Elor <idanelor@apiiro.com>
2022-09-14 12:34:30 +0300

    Add a search user API

---
+++ src/main/java/org/owasp/webgoat/lessons/apiiro/apis/AddAPILesson.java
@@ -0,0 +1,63 @@
+/*
+ * This file is part of WebGoat, an Open Web Application Security Project utility. For details, please see http://www.owasp.org/
+ *
+ * Copyright (c) 2002 - 2019 Bruce Mayhew
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Getting Source ==============
+ *
+ * Source for this application is maintained at https://github.com/WebGoat/WebGoat, a repository for free software projects.
+ */
+
+package org.owasp.webgoat.lessons.apiiro.apis;
+
+import org.jruby.ir.operands.Array;
+import org.owasp.webgoat.container.assignments.AssignmentEndpoint;
+import org.owasp.webgoat.container.assignments.AssignmentHints;
+import org.owasp.webgoat.container.assignments.AttackResult;
+import org.owasp.webgoat.lessons.apiiro.utils.UserDetails;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.tools.Diagnostic;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@RestController
+public class AddAPILesson extends AssignmentEndpoint {
+
+    @GetMapping("/access-control/search-users")
+    @ResponseBody
+    public UserDetails getUserDetails(@RequestParam String user) {
+        UserDetails userDetails = UserDetails.getUser(user);
+        return userDetails;
+    }
+
+}
---
+++ src/main/java/org/owasp/webgoat/lessons/apiiro/utils/UserDetails.java
@@ -0,0 +1,52 @@
+package org.owasp.webgoat.lessons.apiiro.utils;
+
+import java.io.Serializable;
+import java.util.Locale;
+
+import com.github.javafaker.Faker;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class UserDetails implements Serializable {
+    protected String firstName;
+    protected String lastName;
+    protected String ssn;
+    protected String phoneNumber;
+
+    public UserDetails(String firstName, String lastName, String ssn, String phoneNumber) {
+
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.ssn=ssn;
+        this.phoneNumber=phoneNumber;
+        // test
+    }
+
+    public static UserDetails randomUser() {
+        Faker faker = new Faker(new Locale("en-GB"));
+        return new UserDetails(faker.name().firstName(), faker.name().lastName(), faker.number().digits(11), faker.phoneNumber().cellPhone());
+    }
+
+    public static UserDetails getAdmin() {
+        return randomUser();
+    }
+
+    public static UserDetails getUser() {
+        return randomUser();
+    }
+
+    public static UserDetails getUser(String name) {
+        return randomUser();
+    }
+
+    public String getName() {
+            return (this.firstName + " " + this.lastName);
+    }
+
+    public boolean validateCreds() {
+        return true;
+    }
+}
