24746e8ac0f1f7aa562ecf15a9beb78568a3a418
Dave <50186003+thedave42@users.noreply.github.com>
2023-09-12 20:36:51 +0000

    updates for vuln

--- src/main/java/org/owasp/webgoat/lessons/vulnerablecomponents/VulnerableComponentsLesson.java
+++ src/main/java/org/owasp/webgoat/lessons/vulnerablecomponents/VulnerableComponentsLesson.java
@@ -23,8 +23,6 @@
 package org.owasp.webgoat.lessons.vulnerablecomponents;
 
 import com.thoughtworks.xstream.XStream;
-import java.io.ByteArrayInputStream;
-import java.io.ObjectInputStream;
 import org.apache.commons.lang3.StringUtils;
 import org.owasp.webgoat.container.assignments.AssignmentEndpoint;
 import org.owasp.webgoat.container.assignments.AssignmentHints;
@@ -56,10 +54,7 @@ public class VulnerableComponentsLesson extends AssignmentEndpoint {
                 .replace("> ", ">")
                 .replace(" <", "<");
       }
-      ByteArrayInputStream input = new ByteArrayInputStream(payload.getBytes());
-      ObjectInputStream ois = new ObjectInputStream(input);
-      contact = (Contact) ois.readObject();
-      // contact = (Contact) xstream.fromXML(payload);
+      contact = (Contact) xstream.fromXML(payload);
     } catch (Exception ex) {
       return failed(this).feedback("vulnerable-components.close").output(ex.getMessage()).build();
     }
