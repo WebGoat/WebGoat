2290ac1e7541c74abc62a145883548537208cbd4
lisa2000love <50256598+lisa2000love@users.noreply.github.com>
2023-12-03 19:23:21 +0900

    Create create_directories.sh

---
+++ scripts/create_directories.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script is responsible for creating necessary directories
+
+# Navigate to the target directory (replace with the actual path)
+cd /var/www/html
+
+# Create the 'scripts' directory if it doesn't exist
+mkdir -p scripts
