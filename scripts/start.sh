7dba8e2567a6e31d314ab78788d788a684785eb4
lisa2000love <50256598+lisa2000love@users.noreply.github.com>
2023-12-03 23:26:06 +0900

    Create start.sh

---
+++ scripts/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+BUILD_JAR=$(ls /home/ec2-user/build/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
+
+echo "> build 파일 복사" >> /home/ec2-user/deploy.log
+DEPLOY_PATH=/home/ec2-user/
+cp $BUILD_JAR $DEPLOY_PATH
+
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
