ddaa484edf866ab19a7b5096b665999f4be33a0a
lisa2000love <50256598+lisa2000love@users.noreply.github.com>
2023-12-03 20:00:53 +0900

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script is responsible for starting or restarting the application
+
+# Navigate to the application directory
+cd /var/www/html
+
+# Start the application (example command, replace with actual start command)
+java -jar your-application.jar
