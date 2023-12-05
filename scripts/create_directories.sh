2d19deb9ee250792a44e09c9fad489f2f34c7cad
lisa2000love <50256598+lisa2000love@users.noreply.github.com>
2023-12-05 23:16:24 +0900

    Update create_directories.sh

--- scripts/create_directories.sh
+++ scripts/create_directories.sh
@@ -3,7 +3,7 @@
 # This script is responsible for creating necessary directories
 
 # Navigate to the target directory (replace with the actual path)
-cd /var/www/html
+cd /home/ec2-user/
 
 # Create the 'scripts' directory if it doesn't exist
 mkdir -p scripts
