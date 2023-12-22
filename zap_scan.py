5440c4b8fe400b57a166074d7f167c8422dd9092
lisa2000love <50256598+lisa2000love@users.noreply.github.com>
2023-12-22 12:15:22 +0900

    Create zap_scan.py

---
+++ zap_scan.py
@@ -0,0 +1,42 @@
+import subprocess
+import time
+from datetime import datetime
+
+target_sites = [
+    "http://43.200.16.60:8080/WebGoat/"
+]
+
+for site_url in target_sites:
+    # Quick scan
+    zap_command = [
+        "zap-cli", "quick-scan",
+        site_url,
+    ]
+    subprocess.run(zap_command)
+
+    # Generate timestamp for the report filename
+    timestamp = datetime.now().strftime("%Y-%m-%d(%H:%M:%S)")
+    report_filename = f"zap-report#{timestamp}.html"
+
+    # Generate ZAP report in HTML format
+    report_command = [
+        "zap-cli", "report",
+        "-o", f"/home/ec2-user/{report_filename}",
+        "-f", "html",
+    ]
+    subprocess.run(report_command)
+
+    # Upload the report to S3 bucket
+    s3_upload_command = [
+        "aws", "s3", "cp",
+        f"/home/ec2-user/{report_filename}",
+        f"s3://dast-hbucket/{report_filename}",
+    ]
+    subprocess.run(s3_upload_command)
+
+    #rm file
+    time.sleep(10)
+    rm_file_command = [
+        "rm", f"/home/ec2-user/{report_filename}",
+    ]
+    subprocess.run(rm_file_command)
