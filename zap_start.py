c8047314945111b72c3a1affbb390a5d056e5458
lisa2000love <50256598+lisa2000love@users.noreply.github.com>
2023-12-22 12:14:47 +0900

    Create zap_start.py

---
+++ zap_start.py
@@ -0,0 +1,39 @@
+import subprocess
+import time
+
+def kill_process(process_name):
+    try:
+        pid = subprocess.check_output(["pgrep", "-f", process_name]).decode().strip()
+        if pid:
+            print(f"Killing {process_name} with PID {pid}")
+            subprocess.run(["kill", "-9", pid])
+            time.sleep(5)
+        else:
+            print(f"No process found for {process_name}")
+    except subprocess.CalledProcessError:
+        print(f"Error while trying to find or kill {process_name}")
+
+# Kill existing WebGoat process
+kill_process("webgoat-2023.6-SNAPSHOT.jar")
+
+# Kill existing ZAP process
+kill_process("/home/ec2-user/ZAP_2.14.0/zap.sh")
+
+# ZAP
+zap_command = [
+    "/home/ec2-user/ZAP_2.14.0/zap.sh", "-daemon", "-host", "0.0.0.0", "-port", "8090",
+    "-config", "api.disablekey=true",
+    "-config", "api.addrs.addr.name=.*",
+    "-config", "api.addrs.addr.regex=true",
+]
+zap_process = subprocess.Popen(zap_command)
+
+# Sleep for a while to ensure ZAP has started before starting WebGoat
+time.sleep(20)
+
+# Run WebGoat without Docker
+webgoat_command = [
+    "java", "-jar", "webgoat-2023.6-SNAPSHOT.jar",
+]
+webgoat_process = subprocess.Popen(webgoat_command)
+time.sleep(20)
