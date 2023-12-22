4761849a252687fe6f073e520ce5b9de6e233a45
lisa2000love <50256598+lisa2000love@users.noreply.github.com>
2023-12-22 14:32:49 +0900

    Update run_zap.sh

--- run_zap.sh
+++ run_zap.sh
@@ -3,16 +3,7 @@
 # This script is responsible for running OWASP ZAP scan on the application
 
 # Navigate to the OWASP ZAP directory (replace with the actual path)
-cd /home/ec2-user/ZAP_2.10.0
+cd /home/ec2-user/ZAP_2.14.0
 
-# Run OWASP ZAP scan on the target URL (replace with your actual URL)
-./zap.sh -quickurl http://127.0.0.1:8080/WebGoat/start.mvc?username=guest1#lesson/WebGoatIntroduction.lesson
-
-# Save the report to a specific location (replace with the actual path)
-REPORT_PATH="/home/ec2-user/build/zap-report.html"
-./zap.sh -exportreport "$REPORT_PATH" -format html
-
-# Upload the report to S3 (replace with your S3 bucket and path)
-AWS_S3_BUCKET="codebuild-test-hbucket"
-AWS_S3_PATH="reports"
-aws s3 cp "$REPORT_PATH" "s3://$AWS_S3_BUCKET/$AWS_S3_PATH/"
+python3 /home/ec2-user/zap_start.py
+python3 /home/ec2-user/zap_scan.py
