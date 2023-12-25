e471c0ddedda4e216d56b062164cd87158a2d42d
lisa2000love <50256598+lisa2000love@users.noreply.github.com>
2023-12-26 02:15:57 +0900

    Update zap_start.py

--- zap_start.py
+++ zap_start.py
@@ -21,7 +21,7 @@ kill_process("zap.sh")
 
 # ZAP
 zap_command = [
-    "/home/ec2-user/zap.sh", "-daemon", "-host", "0.0.0.0", "-port", "8090",
+    "zap.sh", "-daemon", "-host", "0.0.0.0", "-port", "8090",
     "-config", "api.disablekey=true",
     "-config", "api.addrs.addr.name=.*",
     "-config", "api.addrs.addr.regex=true",
