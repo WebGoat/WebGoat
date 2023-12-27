3c0c272a3f2d671c90cdc0c0b915534284ba3aa2
lisa2000love <50256598+lisa2000love@users.noreply.github.com>
2023-12-27 22:42:09 +0900

    Update zap_scan.py

--- zap_scan.py
+++ zap_scan.py
@@ -5,11 +5,11 @@ from datetime import datetime
 target_sites = [
     "http://43.200.16.60:8080/WebGoat/"
 ]
-time.sleep(30)
+time.sleep(45)
 for site_url in target_sites: 
     # Quick scan
     zap_command = [
-        "./zap-cli", "quick-scan",
+        "./zap/zap-cli", "quick-scan",
         site_url,
     ]
     subprocess.run(zap_command)
@@ -20,7 +20,7 @@ for site_url in target_sites:
 
     # Generate ZAP report in HTML format
     report_command = [
-        "./zap-cli", "report",
+        "./zap/zap-cli", "report",
         "-o", f"./{report_filename}",
         "-f", "html",
     ]
