4777475bb5469ad2c91462f5c0fa761de88b8a4b
lisa2000love <50256598+lisa2000love@users.noreply.github.com>
2023-12-28 15:08:02 +0900

    Update zap_scan.py

--- zap_scan.py
+++ zap_scan.py
@@ -6,10 +6,10 @@ target_sites = [
     "http://43.200.16.60:8080/WebGoat/"
 ]
 
-for site_url in target_sites: 
+for site_url in target_sites:
     # Quick scan
     zap_command = [
-        "zap/zap-cli", "quick-scan",
+        "zap-cli", "quick-scan",
         site_url,
     ]
     subprocess.run(zap_command)
@@ -20,8 +20,8 @@ for site_url in target_sites:
 
     # Generate ZAP report in HTML format
     report_command = [
-        "zap/zap-cli", "report",
-        "-o", f"./{report_filename}",
+        "zap-cli", "report",
+        "-o", f"/home/ec2-user/{report_filename}",
         "-f", "html",
     ]
     subprocess.run(report_command)
@@ -29,14 +29,14 @@ for site_url in target_sites:
     # Upload the report to S3 bucket
     s3_upload_command = [
         "aws", "s3", "cp",
-        f"./{report_filename}",
+        f"/home/ec2-user/{report_filename}",
         f"s3://dast-hbucket/{report_filename}",
     ]
     subprocess.run(s3_upload_command)
 
     #rm file
-    #time.sleep(10)
-    #rm_file_command = [
-    #    "rm", f"/home/ec2-user/{report_filename}",
-    #]
-    #subprocess.run(rm_file_command)
+    time.sleep(10)
+    rm_file_command = [
+        "rm", f"/home/ec2-user/{report_filename}",
+    ]
+    subprocess.run(rm_file_command)
