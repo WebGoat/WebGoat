eba4e094771db40ce157d654222a9ab48444e803
Balevski <sbalevski@kpmg.com>
2024-03-18 10:30:53 +0200

    Test setup

--- mvnw.cmd
+++ mvnw.cmd
@@ -21,7 +21,7 @@
 @REM Maven2 Start Up Batch script
 @REM
 @REM Required ENV vars:
-@REM JAVA_HOME - location of a JDK home dir
+@REM JAVA_HOME - location of a jdk 18home dir
 @REM
 @REM Optional ENV vars
 @REM M2_HOME - location of maven2's installed home dir
---
+++ src/main/java/org/owasp/webgoat/obviousSqlI.java
@@ -0,0 +1,2 @@
+String userId = {get data from end user}; 
+String sqlQuery = "select * from tbluser where userId = " + userId;
