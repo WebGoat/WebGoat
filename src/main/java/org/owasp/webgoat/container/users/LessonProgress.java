fb0c3566bb2c6c736af9503ed861c21bb411adc3
Nanne Baars <nanne.baars@owasp.org>
2024-09-22 20:44:16 +0200

    chore: format code

--- src/main/java/org/owasp/webgoat/container/users/LessonProgress.java
+++ src/main/java/org/owasp/webgoat/container/users/LessonProgress.java
@@ -6,7 +6,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Version;
 import java.util.HashSet;
