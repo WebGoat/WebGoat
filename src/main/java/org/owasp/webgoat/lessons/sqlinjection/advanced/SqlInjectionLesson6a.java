a869ad318f12e449d1c0d8d286c9c37792ad554c
lior-mobb <lior@mobb.ai>
2024-11-08 19:18:07 +0100

    Revert "SQL Injection vulnerability fix (powered by Mobb)-76441256-99ea-4845-9cb5-d82deee39712"
    This reverts commit 90877f30fa720383d079e715884f5e0765b59cfd.

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionLesson6a.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionLesson6a.java
@@ -63,16 +63,15 @@ public class SqlInjectionLesson6a extends AssignmentEndpoint {
     String query = "";
     try (Connection connection = dataSource.getConnection()) {
       boolean usedUnion = true;
-      query = "SELECT * FROM user_data WHERE last_name = ?";
+      query = "SELECT * FROM user_data WHERE last_name = '" + accountName + "'";
       // Check if Union is used
       if (!accountName.matches("(?i)(^[^-/*;)]*)(\\s*)UNION(.*$)")) {
         usedUnion = false;
       }
-      try (PreparedStatement statement =
-          connection.prepareStatement(
-              query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
-        statement.setString(1, accountName);
-        ResultSet results = statement.executeQuery();
+      try (Statement statement =
+          connection.createStatement(
+              ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+        ResultSet results = statement.executeQuery(query);
 
         if ((results != null) && results.first()) {
           ResultSetMetaData resultsMetaData = results.getMetaData();
