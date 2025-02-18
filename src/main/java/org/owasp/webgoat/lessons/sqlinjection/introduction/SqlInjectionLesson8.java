a9a6a18ca23253638f9b7902f51431428ae4bc85
Mobb autofixer <git@mobb.ai>
2025-02-18 11:04:01 +0000

    mobb fix commit: af4fdf33-9df1-4bdb-95bd-e307ad82bdb8

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson8.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson8.java
@@ -65,17 +65,16 @@ public class SqlInjectionLesson8 extends AssignmentEndpoint {
     String query =
         "SELECT * FROM employees WHERE last_name = '"
             + name
-            + "' AND auth_tan = '"
-            + auth_tan
-            + "'";
+            + "' AND auth_tan = ?";
 
     try (Connection connection = dataSource.getConnection()) {
       try {
-        Statement statement =
-            connection.createStatement(
-                ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+        PreparedStatement statement =
+            connection.prepareStatement(
+                query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
         log(connection, query);
-        ResultSet results = statement.executeQuery(query);
+        statement.setString(1, auth_tan);
+        ResultSet results = statement.executeQuery();
 
         if (results.getStatement() != null) {
           if (results.first()) {
