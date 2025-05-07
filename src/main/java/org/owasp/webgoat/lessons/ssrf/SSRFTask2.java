1654a4d358b122e004890fb9b90f1a8a758f1bbb
Mobb autofixer <git@mobb.ai>
2025-05-07 12:48:49 +0000

    Server Side Request Forgery vulnerability fix (powered by Mobb)-59184a45-90d3-4c23-8103-69fd2dec1829
    This change fixes a **medium severity** (🟡) **Server Side Request Forgery** issue reported by **Checkmarx**.
    
    ## Issue description
    Server-Side Request Forgery (SSRF) allows attackers to make unauthorized requests from a vulnerable server, potentially accessing internal systems, services, or data.
    
    ## Fix instructions
    Validate or sanitize user-supplied URLs, ensuring that they are restricted to trusted domains. Implementing proper input validation and using whitelists for acceptable URLs can prevent SSRF attacks.
    
    [More info and fix customization are available in the Mobb platform](http://localhost:5173/organization/290246a8-6c12-40f6-922d-026294a2536c/project/1998a40a-4d9b-474b-824a-0399bd9fc6ba/report/daacb50d-2f6e-483c-8de8-e134e6257fbc/fix/59184a45-90d3-4c23-8103-69fd2dec1829)

--- src/main/java/org/owasp/webgoat/lessons/ssrf/SSRFTask2.java
+++ src/main/java/org/owasp/webgoat/lessons/ssrf/SSRFTask2.java
@@ -48,7 +48,7 @@ public class SSRFTask2 extends AssignmentEndpoint {
   protected AttackResult furBall(String url) {
     if (url.matches("http://ifconfig\\.pro")) {
       String html;
-      try (InputStream in = new URL(url).openStream()) {
+      try (InputStream in = new URL("https://example.com/" + String.valueOf(url).replaceAll("^\\w+://.*?/", "")).openStream()) {
         html =
             new String(in.readAllBytes(), StandardCharsets.UTF_8)
                 .replaceAll("\n", "<br>"); // Otherwise the \n gets escaped in the response
