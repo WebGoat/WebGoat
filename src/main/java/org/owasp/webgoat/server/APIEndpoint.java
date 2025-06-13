8b88910b8241c3b272d8079796e431af749ddc06
Troy Havelock <troy.havelock@snyk.io>
2025-06-13 15:52:18 -0400

    Fixing issue

--- src/main/java/org/owasp/webgoat/server/APIEndpoint.java
+++ src/main/java/org/owasp/webgoat/server/APIEndpoint.java
@@ -1,26 +1,26 @@
-package org.owasp.webgoat.server;
+// package org.owasp.webgoat.server;
 
-import lombok.SneakyThrows;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import lombok.SneakyThrows;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+// import java.net.URI;
+// import java.net.http.HttpClient;
+// import java.net.http.HttpRequest;
+// import java.net.http.HttpResponse;
 
-public class APIEndpoint {
-    @SneakyThrows
-    @GetMapping("/ssrf")
-    public void makeRequest(@RequestParam String url) {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(url))
-            .build();
+// public class APIEndpoint {
+//     @SneakyThrows
+//     @GetMapping("/ssrf")
+//     public void makeRequest(@RequestParam String url) {
+//         HttpClient client = HttpClient.newHttpClient();
+//         HttpRequest request = HttpRequest.newBuilder()
+//             .uri(URI.create(url))
+//             .build();
 
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(System.out::println)
-                .join();
-    }
-}
+//         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+//                 .thenApply(HttpResponse::body)
+//                 .thenAccept(System.out::println)
+//                 .join();
+//     }
+// }
