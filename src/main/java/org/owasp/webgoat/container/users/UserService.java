68cd60d110fcdbdfef1c7631b3c37ec340f789d8
Dev 1 <cwtravis@gmail.com>
2025-07-31 04:40:23 -0400

    Update UserService.java

--- src/main/java/org/owasp/webgoat/container/users/UserService.java
+++ src/main/java/org/owasp/webgoat/container/users/UserService.java
@@ -51,6 +51,7 @@ public class UserService implements UserDetailsService {
 
   private void createLessonsForUser(WebGoatUser webGoatUser) {
     jdbcTemplate.execute("CREATE SCHEMA \"" + webGoatUser.getUsername() + "\" authorization dba");
+    jdbcTemplate.execute("CREATE SCHEMA \"" + webGoatUser.getUsername() + "\" authorization dba");
     flywayLessons.apply(webGoatUser.getUsername()).migrate();
   }
 
