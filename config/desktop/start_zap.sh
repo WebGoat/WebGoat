c80f554149c1353a72b1b9cdf146937ba7a2b5c4
Nanne Baars <nanne.baars@owasp.org>
2025-10-07 09:37:22 +0200

    refactor: make changing versions easier.
    Small refactor of the Dockerfile_desktop file to make it easier to update to newer versions.
    
    Also, the GitHub actions now define the Java version in one place making it easier to update to the latest version.

--- config/desktop/start_zap.sh
+++ config/desktop/start_zap.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
+set -eu
 
-/config/java-jdk/bin/java -jar /config/ZAP_2.15.0/zap-2.15.0.jar
+ZAP_DIR=${ZAP_HOME:-/config/zap}
+
+if [ ! -d "$ZAP_DIR" ]; then
+  echo "ZAP directory $ZAP_DIR not found" >&2
+  exit 1
+fi
+
+ZAP_JAR=$(find "$ZAP_DIR" -maxdepth 1 -name 'zap-*.jar' -print | head -n1)
+
+if [ -z "${ZAP_JAR:-}" ]; then
+  echo "Unable to locate ZAP jar under $ZAP_DIR" >&2
+  exit 1
+fi
+
+exec /config/java-jdk/bin/java -jar "$ZAP_JAR"
