6cceca153fc72e423ff9f4e108008de90d8ca604
René Zubcevic <rene@zubcevic.com>
2026-06-26 10:07:47 +0200

    move lesson to WebGoatIntro

--- src/main/java/org/owasp/webgoat/lessons/webgoatintroduction/Email.java
+++ src/main/java/org/owasp/webgoat/lessons/webgoatintroduction/Email.java
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: Copyright © 2017 WebGoat authors
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
-package org.owasp.webgoat.lessons.webwolfintroduction;
+package org.owasp.webgoat.lessons.webgoatintroduction;
 
 import java.io.Serializable;
 import lombok.Builder;
